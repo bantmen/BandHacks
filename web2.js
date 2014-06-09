0e3317d0564c77e457c540736ba1500004b2420a
cberkay <c.berkayantmen@gmail.com>
2014-06-09 12:37:54 -0400

    transition to express, postgresql and facebook auth

--- web2.js
+++ web2.js
@@ -6,6 +6,7 @@ var express = require("express"),
 	passport = require('passport'),
     FacebookStrategy = require('passport-facebook').Strategy,
 	pg = require('pg');
+	
 
 passport.use(new FacebookStrategy({
 	clientID: "654765054608952",
@@ -20,23 +21,25 @@ passport.use(new FacebookStrategy({
 	}
 ));
 
-var DATABASE_URL = "postgres://lqhwwuagklpoin:AQ_wXUpcw3s6eJXQDdW__CWOj8@ec2-54-197-237-120.compute-1.amazonaws.com:5432/d51f19hl0iptdt";
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
+app.get("/", function(req, res) {
+	var DATABASE_URL = "postgres://lqhwwuagklpoin:AQ_wXUpcw3s6eJXQDdW__CWOj8@ec2-54-197-237-120.compute-1.amazonaws.com:5432/d51f19hl0iptdt";
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('CREATE TABLE User (name email username provider facebook)');
 
   query.on('row', function(row) {
     console.log(JSON.stringify(row));
   });
 });
+})
+
 
 app.use(logfmt.requestLogger());
 
-app.get('/auth/facebook/callback', function(req, res) {
+app.get('/auth/facebook/callback', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
-	res.redirect('/');
-	}
-);
+    res.redirect('/');
+  });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {});
