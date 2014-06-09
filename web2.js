605db56abf1411dd2edd5eb6ddaad069243319e6
cberkay <c.berkayantmen@gmail.com>
2014-06-09 17:24:16 -0400

    a

--- web2.js
+++ web2.js
@@ -36,14 +36,14 @@ app.get("/", function(req, res) {
             client.query('SELECT * FROM User', function (err, result) {
                 if (err) {res.end("ERR-2");}
                 else {
-                    res.write("here");
+                    res.send("here");
                     //res.write(result.rows.length);
                     for (var i = 0; i < result.rows.length; i++) {
                         var row = result.rows[i];
-                        res.write("here");
-                        res.write(row.name);
-                        res.write(row.email);
-                        res.write(row.username);
+                        res.send("here");
+                        res.send(row.name);
+                        res.send(row.email);
+                        res.send(row.username);
                     }
                 }
             });
