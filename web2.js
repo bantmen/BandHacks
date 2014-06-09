b754e0f2684c843ec30a9f25cdf736d04cc86688
cberkay <c.berkayantmen@gmail.com>
2014-06-09 17:10:25 -0400

    a

--- web2.js
+++ web2.js
@@ -24,30 +24,29 @@ passport.use(new FacebookStrategy({
 ));
 
 app.get("/", function(req, res) {
-//	var connectionString = "postgres://lqhwwuagklpoin:AQ_wXUpcw3s6eJXQDdW__CWOj8@ec2-54-197-237-120.compute-1.amazonaws.com:5432/d51f19hl0iptdt";
-//	pg.connect(connectionString, function(err, client) {
-//		if (err) {
-//			res.end("ERR");
-//			console.dir(err);
-//		}
-//		else {
-//			//var query = client.query('CREATE TABLE User (name VARCHAR(40), email VARCHAR(40), username VARCHAR(40) PRIMARY KEY, provider VARCHAR(40), facebook VARCHAR(40))');
-//			client.query('INSERT INTO User (name, email, username, provider, facebook) VALUES ("a", "b", "c", "d", "e")');
-//            client.query('SELECT * FROM User', function (err, result) {
-//                if (err) {res.end("ERR-2");}
-//                else {
-//                    res.write(result.rows.length);
-//                    for (var i = 0; i < result.rows.length; i++) {
-//                        var row = result.rows[i];
-//                        res.write(row.name);
-//                        res.write(row.email);
-//                        res.write(row.username);
-//                    }
-//                }
-//            });
-//			res.end("NO ERR");
-//		}
-    res.end("reached end")
+	var connectionString = "postgres://lqhwwuagklpoin:AQ_wXUpcw3s6eJXQDdW__CWOj8@ec2-54-197-237-120.compute-1.amazonaws.com:5432/d51f19hl0iptdt";
+	pg.connect(connectionString, function(err, client) {
+		if (err) {
+			res.end("ERR");
+			console.dir(err);
+		}
+		else {
+			//var query = client.query('CREATE TABLE User (name VARCHAR(40), email VARCHAR(40), username VARCHAR(40) PRIMARY KEY, provider VARCHAR(40), facebook VARCHAR(40))');
+			client.query('INSERT INTO User (name, email, username, provider, facebook) VALUES ("a", "b", "c", "d", "e")');
+            client.query('SELECT * FROM User', function (err, result) {
+                if (err) {res.end("ERR-2");}
+                else {
+                    res.write(result.rows.length);
+                    for (var i = 0; i < result.rows.length; i++) {
+                        var row = result.rows[i];
+                        res.write(row.name);
+                        res.write(row.email);
+                        res.write(row.username);
+                    }
+                }
+            });
+			res.end("NO ERR");
+		}
 	});
 });
 
