16bf6cf7daccb9b6d444ed32c79f68423a14ab85
cberkay <c.berkayantmen@gmail.com>
2014-06-13 00:28:18 -0400

    working facebook and psql

--- web2.js
+++ web2.js
@@ -12,34 +12,57 @@ app.use(logfmt.requestLogger());
 
 app.use(express.static(__dirname));
 
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 passport.use(new FacebookStrategy({
-	clientID: "654765054608952",
-	clientSecret: "32f8a00e838b2c8aa722eafb936b684c",
+	//clientID : "657777250974399",                   //for test
+	//clientSecret: "6bbd5edcc7d4b9b3ff1da43b8b09239d",
+	clientID : "654765054608952",
+	clientSecret : "32f8a00e838b2c8aa722eafb936b684c",
 	//callbackURL: "http://localhost:5000/auth/facebook/callback"
-	callbackURL: "http://bandhacks.herokuapp.com/auth/facebook/callback"
+	callbackURL : "http://bandhacks.herokuapp.com/auth/facebook/callback"
 	},
 	function(accessToken, refreshToken, profile, done) {
-/* 		client.query('SELECT id FROM "Users" WHERE id=$1', [profile.id], function (err, result) {
+		//var connectionString = "postgres://postgres:root@localhost/postgres";
+		var connectionString = "postgres://qsxtzbqlsljdiy:HVNOigVMRb_JxhP4II7uut1JV9@ec2-54-197-237-231.compute-1.amazonaws.com:5432/dbig67cfjnt8na";
+		pg.connect(connectionString, function (err, client) {
+			client.query('SELECT id FROM "Users" WHERE id=$1', [profile.id], function (err, result) {
 			if (err) {
 				return done(err);
 			}
 			if (result.rows[0]) {
-				return done(err, result.rows[0]);  
+				var user = {username : result.rows[0]};
+				return done(null, result.rows[0]);   
 			 }
 			else {     
-				client.query('INSERT INTO "User" (name, email, username, provider, id)  \
-				VALUES ($1, $2, $3, $4, $5), [profile.displayName, profile.emails[0].value, profile.username, "facebook", profile._json.id]', function (err, result) {
-					if (err) done(err);
-				});
-			}	
-		});  */
-		return done();
+				var sqlParams = [profile.displayName || "no displayname", profile.emails ? profile.emails[0].value : "no email", profile.username || "no username", "facebook", profile._json.id || "problem with _json.id"];
+				var query = client.query('INSERT INTO "Users" (name, email, username, provider, id) VALUES ($1, $2, $3, $4, $5)', sqlParams, function (err, result) {
+					var user = {username : profile.displayName};
+					if (err) {
+						return done(err);
+					}
+					else {
+						return done(null, user);
+						
+					}
+				});	
+			} 
+		});
 	}
-));
+)}));
 
 app.get("/", function(req, res) {
 /* 	var connectionString = "postgres://postgres:root@localhost/postgres";
-	//var connectionString = "postgres://lqhwwuagklpoin:AQ_wXUpcw3s6eJXQDdW__CWOj8@ec2-54-197-237-120.compute-1.amazonaws.com:5432/d51f19hl0iptdt";
+//	var connectionString = "postgres://qsxtzbqlsljdiy:HVNOigVMRb_JxhP4II7uut1JV9@ec2-54-197-237-231.compute-1.amazonaws.com:5432/dbig67cfjnt8na";
 	pg.connect(connectionString, function(err, client) {
 		if (err) {
 			res.end("ERR");
@@ -65,9 +88,7 @@ app.get('/auth/facebook', passport.authenticate('facebook'));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
 									  
 app.get('/login', function(req, res){
-		res.render('profile.ejs', {
-		//
-		});
+        res.end("HELLO WORLD");
 	});
 
 var port = Number(process.env.PORT || 5000);
