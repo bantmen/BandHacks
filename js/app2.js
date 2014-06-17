c8633ad02b9e6f30c35541a55e93878558678855
cberkay <c.berkayantmen@gmail.com>
2014-06-17 19:25:32 -0400

    currently 1.1.5

--- js/app2.js
+++ js/app2.js
@@ -17,22 +17,43 @@ app.controller('TestController', function(){
 });
 
 app.controller('TestController2', function($scope){
+	console.log('first');
 	$scope.test = "hello";
 	var taskList = [];
 	$scope.createTask = function (task) {
-		taskList.push(task);
-		$scope.taskDisplay = "";
-		var temp = "";
-		for (var i=0;i<taskList.length;i++) {
-			temp += (i+1).toString() + "- " + taskList[i] + '<br>';
-			console.log(temp);
-		}	
-		$scope.taskDisplay = temp;
+		if (inputCheck(task)) {
+			console.log('I came');
+			taskList.push(task);
+			$scope.taskDisplay = "";
+			var temp = "";
+			for (var i=0;i<taskList.length;i++) {
+				temp += (i+1).toString() + "- " + taskList[i] + '<br>';
+				console.log(temp);
+			}	
+			$scope.taskDisplay = temp;
+		}
+		else $scope.tempTask = "Please do not use angular brackets.";
+
 	};
-/* 	inputCheck = function(inputString) {
-		if () {}
+	inputCheck = function(inpStr) {
+		if (inpStr.search('<') != -1 && inpStr.search('>') != -1 && inpStr.search('<br>') == 0) {
+			return false;
+		}
 		else return true;
-	}; */
+	};
+	function occurrences(string, subString, allowOverlapping){  //from stackoverflow
+		string+=""; subString+="";
+		if(subString.length<=0) return string.length+1;
+
+		var n=0, pos=0;
+		var step=(allowOverlapping)?(1):(subString.length);
+
+		while(true){
+			pos=string.indexOf(subString,pos);
+			if(pos>=0){ n++; pos+=step; } else break;
+		}
+		return(n);
+	}
 });
 
 app.controller("HelloWorldCtrl", function ($scope){
