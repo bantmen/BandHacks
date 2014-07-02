2fa3732a21360de5cc764c49f2c0186c0900e7a6
cberkay <c.berkayantmen@gmail.com>
2014-07-01 22:10:46 -0400

    fixed syntax for app2

--- js/app2.js
+++ js/app2.js
@@ -163,30 +163,31 @@ var app = angular.module('myApp', ['ngRoute'])
         },1000);
     }; // end fakeProgress
 
-    .controller('addShowCtrl', function($scope) {
-        $scope.modalShown = false;
-        $scope.toggleModal = function() {
-            $scope.modalShown = !$scope.modalShown;
-    };
-});
+})
 
-}) // end dialogsServiceTest
-    .controller('whatsYourNameCtrl',function($scope,$modalInstance,data){
-        $scope.user = {name : ''};
+.controller('addShowCtrl', function($scope) {
+	$scope.modalShown = false;
+	$scope.toggleModal = function() {
+		$scope.modalShown = !$scope.modalShown;
+	};
+})
 
-        $scope.cancel = function(){
-            $modalInstance.dismiss('canceled');
-        }; // end cancel
+.controller('whatsYourNameCtrl',function($scope,$modalInstance,data){
+	$scope.user = {name : ''};
 
-        $scope.save = function(){
-            $modalInstance.close($scope.user.name);
-        }; // end save
+	$scope.cancel = function(){
+		$modalInstance.dismiss('canceled');
+	}; // end cancel
 
-        $scope.hitEnter = function(evt){
-            if(angular.equals(evt.keyCode,13) && !(angular.equals($scope.name,null) || angular.equals($scope.name,'')))
-                $scope.save();
-        }; // end hitEnter
-    }) // end whatsYourNameCtrl
-    .run(['$templateCache',function($templateCache){
-        $templateCache.put('/dialogs/whatsyourname.html','<div class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title"><span class="glyphicon glyphicon-star"></span> User\'s Name</h4></div><div class="modal-body"><ng-form name="nameDialog" novalidate role="form"><div class="form-group input-group-lg" ng-class="{true: \'has-error\'}[nameDialog.username.$dirty && nameDialog.username.$invalid]"><label class="control-label" for="username">Name:</label><input type="text" class="form-control" name="username" id="username" ng-model="user.name" ng-keyup="hitEnter($event)" required><span class="help-block">Enter your full name, first &amp; last.</span></div></ng-form></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">Cancel</button><button type="button" class="btn btn-primary" ng-click="save()" ng-disabled="(nameDialog.$dirty && nameDialog.$invalid) || nameDialog.$pristine">Save</button></div></div></div></div>');
-    }]); // end run / controllers / module
+	$scope.save = function(){
+		$modalInstance.close($scope.user.name);
+	}; // end save
+
+	$scope.hitEnter = function(evt){
+		if(angular.equals(evt.keyCode,13) && !(angular.equals($scope.name,null) || angular.equals($scope.name,'')))
+			$scope.save();
+	}; // end hitEnter
+}) // end whatsYourNameCtrl
+.run(['$templateCache',function($templateCache){
+	$templateCache.put('/dialogs/whatsyourname.html','<div class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title"><span class="glyphicon glyphicon-star"></span> User\'s Name</h4></div><div class="modal-body"><ng-form name="nameDialog" novalidate role="form"><div class="form-group input-group-lg" ng-class="{true: \'has-error\'}[nameDialog.username.$dirty && nameDialog.username.$invalid]"><label class="control-label" for="username">Name:</label><input type="text" class="form-control" name="username" id="username" ng-model="user.name" ng-keyup="hitEnter($event)" required><span class="help-block">Enter your full name, first &amp; last.</span></div></ng-form></div><div class="modal-footer"><button type="button" class="btn btn-default" ng-click="cancel()">Cancel</button><button type="button" class="btn btn-primary" ng-click="save()" ng-disabled="(nameDialog.$dirty && nameDialog.$invalid) || nameDialog.$pristine">Save</button></div></div></div></div>');
+}]); // end run / controllers / module
